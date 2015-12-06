e5e43e7caa723505282b627762b4c4bb70bc5478
Kevin Smith <kevin@operable.io>
2015-12-06 16:17:17 -0500

    Removed unused expr type

--- lib/piper/permissions/ast/unary_expr.ex
+++
@@ -1,9 +0,0 @@
-defmodule Piper.Permissions.Ast.UnaryExpr do
-
-  defstruct [:line, :col, :op, :expr]
-
-  def new({op, {line, col}, _}, expr) do
-    %__MODULE__{line: line, col: col, op: op, expr: expr}
-  end
-
-end
