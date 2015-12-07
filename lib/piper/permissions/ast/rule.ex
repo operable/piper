53d012819064c60062a89c6e4523e5e3536c7772
Kevin Smith <kevin@operable.io>
2015-12-07 09:48:08 -0500

    Handle nil rule references

--- lib/piper/permissions/ast/rule.ex
+++ lib/piper/permissions/ast/rule.ex
@@ -8,8 +8,16 @@ defmodule Piper.Permissions.Ast.Rule do
     %__MODULE__{command_selector: command, permission_selector: perms}
   end
 
+  def command_name(nil) do
+    nil
+  end
   def command_name(rule) do
+    if rule.command_selector != nil and
+       rule.command_selector.right != nil do
     "#{rule.command_selector.right.value}"
+    else
+      nil
+    end
   end
 
 end
