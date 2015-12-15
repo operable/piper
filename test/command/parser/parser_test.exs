9a72329731f36163f563e7e103c9c0a651d082d7
vanstee <patrick@vanstee.me>
2015-12-15 11:42:16 -0500

    Test parsing both short hand and qualified command names

--- test/command/parser/parser_test.exs
+++ test/command/parser/parser_test.exs
@@ -5,54 +5,21 @@ defmodule Parser.ParserTest do
 
   use Parser.ParsingCase
 
-  @commands [{"wubba:foo", "wubba:foo"},
-             {"wubba:foo-bar", "wubba:foo-bar"},
-             {"foo", "foo:foo"},
-             {"foo-bar", "foo-bar:foo-bar"}]
-
-  defmacrop should_parse(expected, actual \\ nil) do
-    if is_nil(actual) do
-      actual = expected
+  defmacrop should_parse(text, ast_text \\ nil, expect \\ true) do
+    if ast_text == nil do
+      ast_text = text
     end
 
-    case {command_used?(expected), command_used?(actual)} do
-      {false, false} ->
-        quote location: :keep do
-          expected_ast = Parser.scan_and_parse(unquote(expected))
-          actual_ast = ast_string(unquote(actual))
-
-          assert matches(expected_ast, actual_ast)
-        end
-      {true, true} ->
-        for {expected_command, actual_command} <- @commands do
-          quote location: :keep do
-            var!(command) = unquote(expected_command)
-            expected_ast = Parser.scan_and_parse(unquote(expected))
-
-            var!(command) = unquote(actual_command)
-            actual_ast = ast_string(unquote(actual))
-
-            assert matches(expected_ast, actual_ast)
-          end
-        end
-      _ ->
-        raise "Both expected and actual arguments must include reference to command"
+    quote location: :keep, bind_quoted: [text: text, ast_text: ast_text, expect: expect] do
+      expected_ast = Parser.scan_and_parse(text)
+      actual_ast = ast_string(ast_text)
+      assert matches(expected_ast, actual_ast) == expect
     end
-  end
-
-  def command_used?(ast) do
-    {_, command_used} = Macro.postwalk(ast, false, fn
-      {:command, _, nil} = t, acc ->
-        {t, acc || true}
-      t, acc ->
-        {t, acc || false}
-    end)
-
-    command_used
   end
 
   test "parsing plain command" do
-    should_parse "#{command}"
+    should_parse "wubba:foo"
+    should_parse "foo", "foo:foo"
   end
 
   test "parsing variable command" do
@@ -60,60 +27,67 @@ defmodule Parser.ParserTest do
   end
 
   test "parsing options" do
-    should_parse "#{command} --bar=1 -f"
+    should_parse "wubba:foo --bar=1 -f"
+    should_parse "foo --bar=1 -f", "foo:foo --bar=1 -f"
+
     should_parse "$foo --bar=1 -f"
-    should_parse "#{command} --tags=\"a,b,c\" 10", "#{command} --tags=a,b,c 10"
+
+    should_parse "ec2:list-vm --tags=\"a,b,c\" 10", "ec2:list-vm --tags=a,b,c 10"
+    should_parse "ec2 --tags=\"a,b,c\" 10", "ec2:ec2 --tags=a,b,c 10"
   end
 
   test "parsing options referring to names" do
-    should_parse "#{command} perms --grant --permission=operable:write --to=bob"
+    should_parse "operable:admin perms --grant --permission=operable:write --to=bob"
+    should_parse "admin perms --grant --permission=operable:write --to=bob", "admin:admin perms --grant --permission=operable:write --to=bob"
   end
 
   test "parsing boolean args" do
-    should_parse "#{command} true"
+    should_parse "foo:bar true"
+    should_parse "foo true", "foo:foo true"
   end
 
   test "parsing variable options" do
-    should_parse "#{command} --vm --tags=$tag"
+    should_parse "ec2:list-vm --tags=$tag"
+    should_parse "ec2 --tags=$tag", "ec2:ec2 --tags=$tag"
   end
 
   test "parsing args" do
-    should_parse "#{command} 123 abc"
+    should_parse "wubba:foo 123 abc"
+    should_parse "foo 123 abc", "foo:foo 123 abc"
   end
 
   test "parsing double quoted string arguments" do
-    should_parse "#{command} \"123 abc\"", "#{command} 123 abc"
+    should_parse "wubba:foo \"123 abc\"", "wubba:foo 123 abc"
+    should_parse "foo \"123 abc\"", "foo:foo 123 abc"
   end
 
   test "parsing single quoted string arguments" do
-    should_parse "#{command} '123 abc'", "#{command} 123 abc"
+    should_parse "wubba:foo '123 abc'", "wubba:foo 123 abc"
+    should_parse "foo '123 abc'", "foo:foo 123 abc"
   end
 
   test "parsing escaped double quoted strings" do
-    should_parse "#{command} \"123\\\"\" abc", "#{command} 123\" abc"
+    should_parse "wubba:foo \"123\\\"\" abc", "wubba:foo 123\" abc"
+    should_parse "foo \"123\\\"\" abc", "foo:foo 123\" abc"
   end
 
   test "parsing escaped single quoted strings" do
-    should_parse "#{command} 123 a\\'b\\'c", "#{command} 123 a \\'b\\'c"
+    should_parse "wubba:foo 123 a\\'b\\'c", "wubba:foo 123 a \\'b\\'c"
+    should_parse "foo 123 a\\'b\\'c", "foo:foo 123 a \\'b\\'c"
   end
 
   test "parsing :pipe pipelines" do
-    should_parse "#{command} 1 --bar | wubba:baz"
-    should_parse "wubba:baz 1 --bar | #{command}"
+    should_parse "wubba:foo 1 --bar | wubba:baz"
+    should_parse "wubba:foo 1 --bar | baz", "wubba:foo 1 --bar | baz:baz"
   end
 
   test "parsing :iff pipelines" do
-    should_parse "#{command} --bar && wubba:baz 1"
-    should_parse "wubba:baz --bar && #{command} 1"
+    should_parse "wubba:foo --bar && wubba:baz 1"
+    should_parse "wubba:foo --bar && baz 1", "wubba:foo --bar && baz:baz 1"
   end
 
   test "parsing combined pipelines" do
-    should_parse "#{command} | wubba:bar 500 --limit=2 | wubba:baz"
-    should_parse "wubba:bar | #{command} 500 --limit=2 | wubba:baz"
+    should_parse "wubba:foo | wubba:bar 500 --limit=2 | wubba:baz"
+    should_parse "foo | bar 500 --limit=2 | baz", "foo:foo | bar:bar 500 --limit=2 | baz:baz"
   end
-
-  test "parsing shorthand command" do
-    should_parse "foo", "foo:foo"
-  end
-
 end
