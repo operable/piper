21daaed1d534e156f28307f5cd5889d0d8f9192a
Kevin Smith <kevin@operable.io>
2016-03-04 14:56:02 -0500

    Simplified to_string for literals

--- lib/piper/command/ast2/chars/literals_chars.ex
+++ lib/piper/command/ast2/chars/literals_chars.ex
@@ -3,18 +3,23 @@ defimpl String.Chars, for: [Piper.Command.Ast2.Integer,
                             Piper.Command.Ast2.Bool] do
 
   def to_string(literal) do
-    value = Map.fetch!(literal, :value)
-    "#{value}"
+    "#{literal.value}"
   end
 
 end
 
 defimpl String.Chars, for: [Piper.Command.Ast2.String] do
 
-  alias Piper.Command.Ast2
+  def to_string(str) do
+    str.value
+  end
 
-  def to_string(%Ast2.String{value: value}) do
-    value
+end
+
+defimpl String.Chars, for: [Piper.Command.Ast2.Emoji] do
+
+  def to_string(emoji) do
+    emoji.value
   end
 
 end
