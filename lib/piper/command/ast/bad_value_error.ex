0bde612289dc9af724c60213a6c66b9afed44fcf
Kevin Smith <kevin@operable.io>
2016-08-16 16:27:00 -0400

    Use JSON encoding for value, if available

--- lib/piper/command/ast/bad_value_error.ex
+++ lib/piper/command/ast/bad_value_error.ex
@@ -3,7 +3,13 @@ defmodule Piper.Command.Ast.BadValueError do
   defexception [:name, :value]
 
   def message(%__MODULE__{name: name, value: value}) do
-    "#{inspect value, pretty: true} bound to #{name} instead of expected scalar."
+    text_value = case Poison.encode(value) do
+                   {:ok, text} ->
+                     text
+                   _ ->
+                     "#{inspect value, pretty: true}"
+                 end
+    "#{text_value} bound to $#{name} instead of expected scalar value."
   end
 
 end
